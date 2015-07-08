2917495bacb45b53dd12828503a9219f7ea296fc
lauual <lauraserraoliva@gmail.com>
2015-07-08 18:03:39 +0200

    Hola2
    Hola2

---
+++ Hemi_Experiment/HemiAttention/Assets/02_Scripts/TextureCreator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureCreator : MonoBehaviour {
+
+	public int resolution = 256;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
